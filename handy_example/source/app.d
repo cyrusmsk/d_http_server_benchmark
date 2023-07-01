c7037e1a3936f5354abd1bcf11505594e644eed5
Serg Gini <kornburn@gmail.com>
2023-07-02 00:55:57 +0400

    Update app.d
    Add check for POST request for handy server

--- handy_example/source/app.d
+++ handy_example/source/app.d
@@ -8,7 +8,7 @@ void main() {
     new HttpServer((ref ctx) {
         if (ctx.request.url == "/") {
             ctx.response.writeBodyString("");
-        } else if (ctx.request.url == "/user") {
+        } else if (ctx.request.url == "/user" && ctx.request.method == Method.POST) {
             ctx.response.writeBodyString("");
         } else if (startsWith(ctx.request.url, "/user/") && ctx.request.method == Method.GET) {
             ctx.response.writeBodyString(ctx.request.url[6..$]);
